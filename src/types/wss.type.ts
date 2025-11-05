29c752c0f8e7df2cbaf4ecd7bd76946526b55b11
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-05 18:28:16 +0900

    feat: Add type for Binance Websocket

---
+++ src/types/wss.type.ts
@@ -0,0 +1,24 @@
+export interface CryptoEventType {
+  e: string;
+  E: number;
+  s: string;
+  k: {
+    t: number;
+    T: number;
+    s: string;
+    i: string;
+    f: number;
+    L: number;
+    o: string;
+    c: string;
+    h: string;
+    l: string;
+    v: string;
+    n: number;
+    x: boolean;
+    q: string;
+    V: string;
+    Q: string;
+    B: string;
+  };
+}
