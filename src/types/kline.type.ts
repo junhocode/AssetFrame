8dcf78fdbdcd04ac239a3f64382b8ed97a6cbd7c
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-06 16:11:24 +0900

    refactor: Add both types for rest and ws including comments

--- src/types/kline.type.ts
+++ src/types/kline.type.ts
@@ -1,18 +1,32 @@
 import type { CandlestickData, HistogramData, Time } from "lightweight-charts";
 
+export interface BinanceWSKline {
+  e: string; // Event type 
+  E: number; // Event time    
+  s: string; // Symbol     
+  a: number; // Aggregate trade ID
+  p: string; // Price
+  q: string; // Quantity
+  f: number; // First trade ID
+  l: number; // Last trade ID
+  T: number; // Trade time
+  m: boolean; // Is the buyer the market maker?
+  M: boolean; // Ignore
+}
+
 export type BinanceRestKline = [
-  number,   
-  string,  
-  string,   
-  string,  
-  string,   
-  string, 
-  number,   
-  string,   
-  number,   
-  string,   
-  string,   
-  string   
+  number,   // Kline open time
+  string,   // Open price
+  string,   // High price
+  string,   // Low price
+  string,   // Close price
+  string,   // Volume
+  number,   // Kline Close time
+  string,   // Quote asset volume
+  number,   // Number of trades
+  string,   // Taker buy base asset volume
+  string,   // Taker buy quote asset volume
+  string    // Unused field, ignore.
 ];
 
 export interface GetKlinesParams {
