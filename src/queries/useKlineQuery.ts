cf83b536771757336c62d637045aebf0de55ca34
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-06 15:29:38 +0900

    feat: Add useQuery for volumes

--- src/queries/useKlineQuery.ts
+++ src/queries/useKlineQuery.ts
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { QUERY_KEYS } from '@/constants/queryKeys';
 import { getKlines } from '@/apis/klines.api';
-import { parseRestKlineToCandle } from '@/components/utils/klineParser';
+import { parseRestKlineToCandle, parseRestKlineToVolume } from '@/components/utils/klineParser';
 import type { GetKlinesParams } from '@/types/kline.type';
 
 export const useCandlesQuery = (params: GetKlinesParams) => {
@@ -11,6 +11,20 @@ export const useCandlesQuery = (params: GetKlinesParams) => {
     queryFn: async () => {
       const rawData = await getKlines(params);
       const parsedData = rawData.map(parseRestKlineToCandle);
+      return parsedData;
+    },
+    
+    enabled: !!params.symbol,
+  });
+};
+
+export const useVolumesQuery = (params: GetKlinesParams) => {
+  return useQuery({
+    queryKey: QUERY_KEYS.volumes.list(params),
+
+    queryFn: async () => {
+      const rawData = await getKlines(params);
+      const parsedData = rawData.map(parseRestKlineToVolume);
       return parsedData;
     },
     
