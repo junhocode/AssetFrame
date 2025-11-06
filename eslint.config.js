968dfe38843e44c4ea2224d87b09fac2fa3675b6
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-06 14:32:15 +0900

    feat: Add fetcher for api

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,9 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+     "rules": {
+        "@typescript-eslint/no-explicit-any": "off", 
+        "@typescript-eslint/explicit-module-boundary-types": "off",
+    }
   },
 ])
