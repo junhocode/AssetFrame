c11bda2bd0d82394ed316efd9de55dd9ef37cec1
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-06 18:22:11 +0900

    refactor: Refactor api urls using proxy

--- src/apis/klines.api.ts
+++ src/apis/klines.api.ts
@@ -1,10 +1,10 @@
 import fetcher from "./fetcher.api";
-import { BASE_URL, ENDPOINT } from "./url.api";
+import { ENDPOINT } from "./url.api";
 import type { BinanceRestKline, GetKlinesParams } from "@/types/kline.type"
 
 export const getKlines = async ({ symbol, interval, limit }: GetKlinesParams): Promise<BinanceRestKline[]> => {
   return fetcher.get<BinanceRestKline[]>({
-    url: BASE_URL + ENDPOINT.KLINE,
+    url: ENDPOINT.KLINE,
     params: { symbol, interval, limit}
   });
 };
--- src/apis/url.api.ts
+++ src/apis/url.api.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = import.meta.env.VITE_BINANCE_HTTP_URL
+export const BASE_URL = import.meta.env.VITE_FRONTEND_URL
 
 export const ENDPOINT = {
-    KLINE: '/api/v3/klines'
+    KLINE: '/proxy-api/api/v3/klines'
 }
