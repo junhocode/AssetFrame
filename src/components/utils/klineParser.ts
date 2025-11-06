38aac35c218c655b3deada77706c19f5c2c37fbc
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-06 15:28:23 +0900

    feat: Add data parser for both rest and ws

---
+++ src/components/utils/klineParser.ts
@@ -0,0 +1,34 @@
+import type { CryptoEventType } from "@/types/wss.type";
+import type { CandleData, VolumeData, BinanceRestKline } from "@/types/kline.type";
+
+export const parseWsKlineToCandle = (kline: CryptoEventType): CandleData => ({
+  time: (kline.k.t / 1000) as CandleData['time'],
+  open: parseFloat(kline.k.o),
+  high: parseFloat(kline.k.h),
+  low: parseFloat(kline.k.l),
+  close: parseFloat(kline.k.c),
+});
+
+export const parseWsKlineToVolume = (kline: CryptoEventType): VolumeData => ({
+  time: (kline.k.t / 1000) as VolumeData['time'],
+  value: parseFloat(kline.k.v),
+  color: parseFloat(kline.k.c) >= parseFloat(kline.k.o) 
+    ? 'rgba(0, 150, 136, 0.8)' 
+    : 'rgba(255, 82, 82, 0.8)',
+});
+
+export const parseRestKlineToCandle = (kline: BinanceRestKline): CandleData => ({
+  time: (kline[0] / 1000) as CandleData['time'],
+  open: parseFloat(kline[1]),
+  high: parseFloat(kline[2]),
+  low: parseFloat(kline[3]),
+  close: parseFloat(kline[4]),
+});
+
+export const parseRestKlineToVolume = (kline: BinanceRestKline): VolumeData => ({
+  time: (kline[0] / 1000) as VolumeData['time'],
+  value: parseFloat(kline[5]),
+  color: parseFloat(kline[4]) >= parseFloat(kline[1]) 
+    ? 'rgba(0, 150, 136, 0.8)' 
+    : 'rgba(255, 82, 82, 0.8)',
+});
