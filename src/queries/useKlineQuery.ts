2516a1993c44ccb6390ad2a8c72b6f02cb6a60b8
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-07 14:41:15 +0900

    feat: Change queries to infiniteQueries

--- src/queries/useKlineQuery.ts
+++ src/queries/useKlineQuery.ts
@@ -1,25 +1,41 @@
-import { useQuery } from '@tanstack/react-query';
-import { QUERY_KEYS } from '@/constants/queryKeys';
-import { getKlines } from '@/apis/klines.api';
-import { parseRestKlineToCandle, parseRestKlineToVolume } from '@/components/utils/klineParser';
-import type { GetKlinesParams } from '@/types/kline.type';
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { QUERY_KEYS } from "@/constants/queryKeys";
+import { getKlines } from "@/apis/klines.api";
+import {
+  parseRestKlineToCandle,
+  parseRestKlineToVolume,
+} from "@/components/utils/klineParser";
+import type { GetKlinesParams } from "@/types/kline.type";
 
-export const useCandlesQuery = (params: GetKlinesParams) => {
-  return useQuery({
+export const useInfiniteCandlesQuery = (params: GetKlinesParams) => {
+  return useInfiniteQuery({
     queryKey: QUERY_KEYS.candles.list(params),
 
-    queryFn: async () => {
-      const rawData = await getKlines(params);
+    queryFn: async ({ pageParam }) => {
+      const rawData = await getKlines({ ...params, endTime: pageParam });
       const parsedData = rawData.map(parseRestKlineToCandle);
       return parsedData;
     },
-    
+
+    getNextPageParam: (lastPage) => {
+      if (typeof params.limit !== "number") {
+        return undefined;
+      }
+      if (lastPage.length < params.limit) {
+        return undefined;
+      }
+
+      const oldestCandleTime = lastPage[0].time as number;
+
+      return oldestCandleTime;
+    },
+
     enabled: !!params.symbol,
   });
 };
 
-export const useVolumesQuery = (params: GetKlinesParams) => {
-  return useQuery({
+export const useInfiniteVolumesQuery = (params: GetKlinesParams) => {
+  return useInfiniteQuery({
     queryKey: QUERY_KEYS.volumes.list(params),
 
     queryFn: async () => {
@@ -27,8 +43,20 @@ export const useVolumesQuery = (params: GetKlinesParams) => {
       const parsedData = rawData.map(parseRestKlineToVolume);
       return parsedData;
     },
-    
+
+    getNextPageParam: (lastPage) => {
+      if (typeof params.limit !== "number") {
+        return undefined;
+      }
+      if (lastPage.length < params.limit) {
+        return undefined;
+      }
+
+      const oldestVolumeTime = lastPage[0].time as number;
+
+      return oldestVolumeTime;
+    },
+
     enabled: !!params.symbol,
   });
 };
-
