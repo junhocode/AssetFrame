24229de9e0c39bd6442fbb562e4fb3e4a3139eea
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-07 17:13:19 +0900

    fixed: Fix temp chartParam using useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react';
 import KlineChart from './components/KlineChart/KlineChart';
 import { useRealtimeChartData } from '@/hooks/useRealtimeChartData';
-import type { GetKlinesParams } from '@/types/kline.type';
 
 export default function App() {
-  const [chartParams, setChartParams] = useState<GetKlinesParams>({
+  const chartParams = {
     symbol: 'BTCUSDT',
     interval: '1m',
-  });
+    limit: 200
+  };
 
   useRealtimeChartData(chartParams);
 
