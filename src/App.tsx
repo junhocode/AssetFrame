b0aa8b9af9cf0b34623d03e25efaa6fad2222355
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-08 17:26:53 +0900

    feat: Add getting symbols dynamically in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import KlineChart from './components/KlineChart/KlineChart';
 import { useRealtimeChartData } from '@/hooks/useRealtimeChartData';
 import { ItemSelector } from './components/ItemSelector/ItemSelector';
 
 export default function App() {
+  const [symbol, setSymbol] = useState<string>('BTCUSDT');
+
   const chartParams = {
-    symbol: 'BTCUSDT',
+    symbol: symbol,
     interval: '1m',
     limit: 200
   };
 
   useRealtimeChartData(chartParams);
 
+  const handleSymbolChange = (symbol: string) => {
+    setSymbol(symbol)
+  }
+
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gray-500">
       <div className="flex flex-col items-start gap-4">
-        <ItemSelector />
+        <ItemSelector value={symbol} onChange={handleSymbolChange}/>
         <KlineChart params={chartParams} />
       </div>
     </div>
