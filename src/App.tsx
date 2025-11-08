de29fe1a2dad173fbff842d28ae10c8569d8a50b
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-08 21:30:39 +0900

    feat: Add TimeScaleSelector in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react';
 import KlineChart from './components/KlineChart/KlineChart';
 import { useRealtimeChartData } from '@/hooks/useRealtimeChartData';
 import { ItemSelector } from './components/ItemSelector/ItemSelector';
+import { TimeScaleSelector } from './components/TimeScaleSelector/TimeScaleSelector';
 
 export default function App() {
   const [symbol, setSymbol] = useState<string>('BTCUSDT');
+  const [timeScale, setTimeScale] = useState<string>('1m');
 
   const chartParams = {
     symbol: symbol,
-    interval: '1m',
+    interval: timeScale,
     limit: 200
   };
 
@@ -18,10 +20,17 @@ export default function App() {
     setSymbol(symbol)
   }
 
+  const handleTimeScaleChange = (timeScale: string) => {
+    setTimeScale(timeScale)
+  }
+
   return (
     <div className="flex justify-center items-center h-screen w-screen bg-gray-500">
       <div className="flex flex-col items-start gap-4">
+        <div className='flex gap-3'>
         <ItemSelector value={symbol} onChange={handleSymbolChange}/>
+        <TimeScaleSelector value={timeScale} onChange={handleTimeScaleChange}/>
+        </div>
         <KlineChart params={chartParams} />
       </div>
     </div>
