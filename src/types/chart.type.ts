b83007adbaffc20f3abf013677cdf81112b6958e
진준호 <theworstvalentine@gmail.com>
2025-11-09 14:29:37 +0900

    feat: Add MovingAverage types

--- src/types/chart.type.ts
+++ src/types/chart.type.ts
@@ -1,5 +1,23 @@
+import type { IChartApi, SeriesOptionsCommon, CandlestickData } from "lightweight-charts";
 import type { GetKlinesParams } from "./kline.type";
 
-export interface KlineChartProps {
-    params: GetKlinesParams;
+interface MovingAverageHookOptions extends SeriesOptionsCommon {
+    length: number;
   }
+
+export interface KlineChartProps {
+  params: GetKlinesParams;
+}
+
+export interface MovingAverageFunctionProps {
+  candleData: CandlestickData[];
+  maLength: number;
+}
+
+export interface MovingAverageHookProps {
+  chart: IChartApi | null,
+  data: CandlestickData[],
+  options: MovingAverageHookOptions,
+  visible: boolean
+}
+
