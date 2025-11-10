8b466b2cf44f2da7c60bdd17bb020c5c2fcce06b
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-10 17:41:20 +0900

    fixed: Fix calculating before visible

--- src/hooks/useMovingAverage.ts
+++ src/hooks/useMovingAverage.ts
@@ -4,31 +4,40 @@ import type { MovingAverageHookProps } from "@/types/chart.type";
 import { calculateMovingAverageSeriesData } from "@/utils/movingAverageCalculator";
 
 export const useMovingAverage = ({ chart, data, options, visible }: MovingAverageHookProps) => {
-    const seriesRef = useRef<ISeriesApi<'Line'> | null>(null);
+  const seriesRef = useRef<ISeriesApi<'Line'> | null>(null);
 
   useEffect(() => {
     if (!chart || data.length === 0) {
         return;
     }
 
-    if (!seriesRef.current) {
-      const { ...seriesOptions } = options; 
-      seriesRef.current = chart.addLineSeries({
-        ...seriesOptions,
-        lastValueVisible: false, 
-        priceLineVisible: false,
-      });
+     if (seriesRef.current) {
+      seriesRef.current.applyOptions({ visible });
     }
-
-    seriesRef.current.applyOptions({ visible });
 
     if (visible) {
       const maData = calculateMovingAverageSeriesData({
           candleData: data,
           maLength: options.length
       });
-      seriesRef.current.setData(maData);
+      
+      if (maData && maData.length > 0) {
+        seriesRef.current?.setData(maData)
+      } else {
+        seriesRef.current?.setData([]);
+      }
     }
+
+    return () => {
+      if (chart && seriesRef.current) {
+        try {
+          chart.removeSeries(seriesRef.current);
+        } catch {
+          //empty block
+        }
+      }
+      seriesRef.current = null;
+    };
     
   }, [chart, data, options, visible]); 
 };
