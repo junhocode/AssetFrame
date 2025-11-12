769d79f31d655a10cc020b3821741e7c4ed83814
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-12 14:40:03 +0900

    feat: klineParser now parses websoket data into rest data

--- src/utils/klineParser.ts
+++ src/utils/klineParser.ts
@@ -1,34 +1,19 @@
+import type { BinanceRestKline } from "@/types/kline.type";
 import type { BinanceWSKline } from "@/types/ws.type";
-import type { CandleData, VolumeData, BinanceRestKline } from "@/types/kline.type";
 
-export const parseWsKlineToCandle = (klineData: BinanceWSKline['k']): CandleData => ({
-  time: (klineData.t / 1000) as CandleData['time'],
-  open: parseFloat(klineData.o),
-  high: parseFloat(klineData.h),
-  low: parseFloat(klineData.l),
-  close: parseFloat(klineData.c),
-});
-
-export const parseWsKlineToVolume = (klineData: BinanceWSKline['k']): VolumeData => ({
-  time: (klineData.t / 1000) as VolumeData['time'],
-  value: parseFloat(klineData.v),
-  color: parseFloat(klineData.c) >= parseFloat(klineData.o) 
-    ? 'rgba(0, 150, 136, 0.8)' 
-    : 'rgba(255, 82, 82, 0.8)',
-});
-
-export const parseRestKlineToCandle = (kline: BinanceRestKline): CandleData => ({
-  time: (kline[0] / 1000) as CandleData['time'],
-  open: parseFloat(kline[1]),
-  high: parseFloat(kline[2]),
-  low: parseFloat(kline[3]),
-  close: parseFloat(kline[4]),
-});
-
-export const parseRestKlineToVolume = (kline: BinanceRestKline): VolumeData => ({
-  time: (kline[0] / 1000) as VolumeData['time'],
-  value: parseFloat(kline[5]),
-  color: parseFloat(kline[4]) >= parseFloat(kline[1]) 
-    ? 'rgba(0, 150, 136, 0.8)' 
-    : 'rgba(255, 82, 82, 0.8)',
-});
+export const parseWsKlineToRaw = (wsKline: BinanceWSKline['k']): BinanceRestKline => {
+  return [
+    wsKline.t,       
+    wsKline.o,       
+    wsKline.h,    
+    wsKline.l,    
+    wsKline.c,       
+    wsKline.v,       
+    wsKline.T,       
+    wsKline.q,       
+    wsKline.n,      
+    "0",           
+    "0",             
+    "0",             
+  ];
+};
