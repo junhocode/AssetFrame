40e5caf4736576052dcda0eab38d5460ca114e51
진준호 <theworstvalentine@gmail.com>
2025-11-14 14:52:17 +0900

    feat: useInfiniteKlineQuery gets keepPreviousData

--- src/queries/useInfiniteKlineQuery.ts
+++ src/queries/useInfiniteKlineQuery.ts
@@ -2,13 +2,26 @@ import {
   useInfiniteQuery,
   type QueryKey,
   type InfiniteData,
+  type UseInfiniteQueryOptions,
 } from "@tanstack/react-query";
 import { QUERY_KEYS } from "@/constants/queryKeys";
 import { getKlines } from "@/apis/klines.api";
 import type { GetKlinesParams } from "@/types/kline.type";
 import type { KlinesData } from "@/types/kline.type";
 
-export const useInfiniteKlinesQuery = (params: GetKlinesParams) => {
+export const useInfiniteKlinesQuery = (
+  params: GetKlinesParams,
+  options?: Omit<
+    UseInfiniteQueryOptions<
+      KlinesData,
+      Error,
+      InfiniteData<KlinesData>,
+      QueryKey,
+      number | undefined
+    >,
+    'queryKey' | 'queryFn' | 'getNextPageParam' | 'initialPageParam'
+  >
+) => {
   return useInfiniteQuery<
     KlinesData,
     Error,
@@ -20,7 +33,7 @@ export const useInfiniteKlinesQuery = (params: GetKlinesParams) => {
 
     queryFn: async ({ pageParam }) => {
       const rawData = await getKlines({ ...params, endTime: pageParam });
-      return { klines: rawData };                      
+      return { klines: rawData };
     },
 
     getNextPageParam: (lastPage) => {
@@ -41,5 +54,7 @@ export const useInfiniteKlinesQuery = (params: GetKlinesParams) => {
 
     initialPageParam: undefined,
     enabled: !!params.symbol && typeof params.limit === "number",
+    
+    ...options,
   });
 };
