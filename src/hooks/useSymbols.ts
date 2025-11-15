4d77e45c84fc9cb0e7bc14ae24722d0a777c7081
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-15 23:31:52 +0900

    feat: Add useSymbols

---
+++ src/hooks/useSymbols.ts
@@ -0,0 +1,17 @@
+import { useMemo } from "react";
+import { useSymbolsQuery } from "@/queries/useSymbolsQuery";
+
+export function useSymbols(searchQuery: string) {
+  const { data: allSymbols, isLoading, isError } = useSymbolsQuery();
+
+  const filteredSymbols = useMemo(() => {
+    if (!allSymbols) return [];
+    if (!searchQuery) return allSymbols;
+
+    return allSymbols.filter((symbol) =>
+      symbol.label.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [allSymbols, searchQuery]);
+
+  return { allSymbols, filteredSymbols, isLoading, isError };
+}
