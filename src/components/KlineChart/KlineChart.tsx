2580e5097b24d9c9832510ffa94662f739ed45ff
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-17 16:35:33 +0900

    feat: add randomColorGenarator

--- src/components/KlineChart/KlineChart.tsx
+++ src/components/KlineChart/KlineChart.tsx
@@ -17,15 +17,6 @@ import {
 import type { KlineChartProps } from "@/types/chart.type";
 import * as S from "./KlineChart.styles";
 
-function getRandomColor(): string {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
 export default function KlineChart({
   data,
   fetchNextPage,
