be20fd94a19e7eb2703ca12dc5835a319e1922de
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-22 11:40:06 +0900

    feat: remove select, use whitelist in queryFn

--- src/queries/useSymbolsQuery.ts
+++ src/queries/useSymbolsQuery.ts
@@ -1,38 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { getSymbols } from "@/apis/symbols.api";
 import { QUERY_KEYS } from "@/constants/queryKeys";
-import type { BinanceRestExchangeInfoResponse, SymbolData } from "@/types/symbol.type";
+import { WHITELISTED_SYMBOLS } from "@/constants/whiteList";
+import { getTickers } from "@/apis/ticker.api";
+import type { BinanceTickerData } from "@/types/ticker.type";
 
 export const useSymbolsQuery = () => {
-  return useQuery<BinanceRestExchangeInfoResponse, Error, SymbolData[]>({
-    queryKey: QUERY_KEYS.symbols.list(),
+  return useQuery<BinanceTickerData[], Error>({
+    queryKey: QUERY_KEYS.symbols.whitelist(),
     
-    queryFn: getSymbols,
+    queryFn: () => getTickers(WHITELISTED_SYMBOLS),
     
-    select: (data) => {
-      const symbolsArray = data.symbols;
-
-      if (!Array.isArray(symbolsArray)) {
-        return [];
-      }
-      
-      const activeSymbols = symbolsArray.filter(
-        (s) => 
-          s.status === "TRADING" && 
-          s.isSpotTradingAllowed &&
-          s.quoteAsset === 'USDT'
-      );
-
-      const formattedSymbols: SymbolData[] = activeSymbols.map((s) => ({
-        value: s.symbol, 
-        label: `${s.baseAsset}/${s.quoteAsset}`, 
-      }));
-
-      return formattedSymbols;
-    },
-
-    staleTime: 1000 * 60 * 60 * 24,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
+    refetchInterval: 5000,
+    staleTime: 4000, 
+    refetchOnWindowFocus: true,
   });
 };
