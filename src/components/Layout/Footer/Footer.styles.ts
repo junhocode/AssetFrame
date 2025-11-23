86154c05e45815972a8b414eb6355da0487c91ab
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-23 19:59:04 +0900

    feat: add Footer

---
+++ src/components/Layout/Footer/Footer.styles.ts
@@ -0,0 +1,3 @@
+export const footer = "w-full h-6 flex justify-center items-center";
+
+export const text = "text-sm text-gray-400 mb-1.5";
---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import * as S from "./Footer.styles"
+
+const Footer = () => {
+    return (
+      <div className={S.footer}>
+        <div className={S.text}>Copyright © 2025 Developed by JunhoCode, Powered by</div>
+        <img src="/Binance-Logo.png" alt="Binance" 
+        className="h-4 object-contain mb-2 ml-1"/>
+      </div>
+    );
+  };
+  
+  export default Footer;
