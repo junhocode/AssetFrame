b6460088d41f293702bb7ba9c844c447647c0186
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-23 20:02:28 +0900

    refactor: change CharPage into main which will be located between header and footer

---
+++ src/components/Layout/Main/Main.styles.ts
@@ -0,0 +1,13 @@
+export const container =
+  "flex h-full w-screen bg-neutral-900 overflow-hidden"; 
+
+export const set = 
+  "flex flex-col flex-1 h-full p-4 gap-4 min-w-0"; 
+
+export const selectors = "flex gap-3 items-center flex-wrap";
+
+export const loading =
+  "flex flex-col justify-center items-center gap-4 h-screen w-screen";
+
+export const fetching =
+  "absolute inset-0 z-10 flex flex-col justify-center items-center gap-2 bg-black/50 backdrop-blur-sm text-white";
