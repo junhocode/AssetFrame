2df6a7a22c7a79d57077a7d94d9f90666b32a763
진준호 <161407999+junhocode@users.noreply.github.com>
2025-11-23 19:55:36 +0900

    feat: add useOrderBookQuery

---
+++ src/queries/useOrderBookQuery.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { getOrderBooks } from "@/apis/orderBook.api";
+import { QUERY_KEYS } from "@/constants/queryKeys";
+import type { GetOrderBookResponse } from "@/types/orderBook.type";
+
+export const useOrderBookQuery = (symbol: string) => {
+  return useQuery<GetOrderBookResponse, Error>({
+    queryKey: QUERY_KEYS.orderBook.detail(symbol),
+    queryFn: () => getOrderBooks({ symbol, limit: 20 }),
+    refetchInterval: false,
+    staleTime: 4000,
+    refetchOnWindowFocus: true,
+  });
+};
