7f94dbd565da63dc63a04c88b144262ba5f4a041
진준호 <161407999+junhocode@users.noreply.github.com>
2025-12-13 13:13:30 +0900

    style: style change of queries

--- src/queries/useSymbolsQuery.ts
+++ src/queries/useSymbolsQuery.ts
@@ -1,14 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
+import { getTickers } from "@/apis/ticker.api";
 import { QUERY_KEYS } from "@/constants/queryKeys";
 import { WHITELISTED_SYMBOLS } from "@/constants/whiteList";
-import { getTickers } from "@/apis/ticker.api";
-import type { BinanceTickerData } from "@/types/ticker.type";
+import type { BinanceTicker } from "@/types/ticker.type";
 
 export const useSymbolsQuery = () => {
-  return useQuery<BinanceTickerData[], Error>({
+  return useQuery<BinanceTicker[], Error>({
     queryKey: QUERY_KEYS.symbols.whitelist(),
 
     queryFn: () => getTickers(WHITELISTED_SYMBOLS),
+    
     refetchOnWindowFocus: true,
   });
 };
