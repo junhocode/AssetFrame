7962480eefd23ae9e542fa82c4c8396548bbba55
진준호 <161407999+junhocode@users.noreply.github.com>
2025-12-13 13:12:36 +0900

    style: layout style change of api

--- src/apis/ticker.api.ts
+++ src/apis/ticker.api.ts
@@ -1,16 +1,10 @@
 import fetcher from "./fetcher.api";
 import { ENDPOINT } from "./url.api";
-import type { BinanceTickerData } from "@/types/ticker.type";
+import type { BinanceTicker } from "@/types/ticker.type";
 
-export const getTickers = async (
-  symbols: string[]
-): Promise<BinanceTickerData[]> => {
-  const symbolsParam = JSON.stringify(symbols);
-
-  return fetcher.get<BinanceTickerData[]>({
+export const getTickers = async (symbols: string[]): Promise<BinanceTicker[]> => {
+  return fetcher.get<BinanceTicker[]>({
     url: ENDPOINT.TICKER,
-    params: {
-      symbols: symbolsParam,
-    },
+    params: { symbols }
   });
 };
