557d99f10c5feb4a2f55ab7fce81cacbaea510f1
junhocode <161407999+junhocode@users.noreply.github.com>
2026-02-26 22:52:54 +0900

    refactor: Refactor useTickers

--- src/hooks/useTickers.ts
+++ src/hooks/useTickers.ts
@@ -1,22 +1,28 @@
 import { useMemo } from "react";
-import { useTickersQuery } from "@/queries/useTickerQuery";
+import { useTickerQuery } from "@/queries/useTickerQuery";
 import { WHITELISTED_CRYPTOS } from "@/constants/whiteList";
 import type { DisplayTicker } from "@/types/ticker.type";
 
 export const useTickers = () => {
-  const { data: tickersData, isLoading, isError } = useTickersQuery();
+  const { data: tickersData, isLoading, isError } = useTickerQuery();
 
   const allTickers: DisplayTicker[] = useMemo(() => {
-    const tickersMap = new Map(
-      tickersData?.map((ticker) => [ticker.symbol, ticker]) || []
-    );
+    if (!tickersData) {
+      return WHITELISTED_CRYPTOS.map(coin => ({ ...coin, value: coin.symbol }));
+    }
 
-    return WHITELISTED_CRYPTOS.map((coin) => ({
-      ...coin,
-      value: coin.symbol,
-      lastPrice: tickersMap.get(coin.symbol)?.lastPrice,
-      priceChangePercent: tickersMap.get(coin.symbol)?.priceChangePercent,
-    }));
+    const tickersMap = new Map(tickersData.map((t) => [t.symbol, t]));
+
+    return WHITELISTED_CRYPTOS.map((coin) => {
+      const ticker = tickersMap.get(coin.symbol);
+      
+      return {
+        ...coin,
+        value: coin.symbol,
+        lastPrice: ticker?.lastPrice,
+        priceChangePercent: ticker?.priceChangePercent,
+      };
+    });
   }, [tickersData]);
 
   return { allTickers, isLoading, isError };
