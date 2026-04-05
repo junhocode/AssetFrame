7c7c609045f1b5569137d1797f14b4279df49abc
진준호 <theworstvalentine@gmail.com>
2026-04-05 17:36:51 +0900

    feat: Add Indicators and intervals in WHITELIST

--- src/constants/whiteList.ts
+++ src/constants/whiteList.ts
@@ -31,3 +31,15 @@ export const WHITELISTED_CRYPTOS: DisplayTicker[] = [
 export const WHITELISTED_SYMBOLS: string[] = WHITELISTED_CRYPTOS.map(
   (coin) => coin.symbol
 );
+
+export const INDICATORS = [
+  { value: "ATR", label: "Average True Range (ATR)" },
+  { value: "BollingerBands", label: "Bollinger Bands (BB)" },
+  { value: "EMA", label: "Exponential Moving Average (EMA)" },
+  { value: "MACD", label: "Moving Average Convergence Divergence (MACD)" },
+  { value: "RSI", label: "Relative Strength Index (RSI)" },
+  { value: "SMA", label: "Simple Moving Average (SMA)" },
+  { value: "Stochastic", label: "Stochastic Oscillator (KD)" },
+];
+
+export const INTERVALS = ["1s", "1m", "15m", "1h", "4h", "1d", "1w", "1M"];
