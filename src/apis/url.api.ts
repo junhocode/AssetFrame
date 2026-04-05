3ceb62a049ce633670881a2e934cfe06ab129b19
진준호 <theworstvalentine@gmail.com>
2026-04-05 17:21:41 +0900

    refactor: Rest Api url is now controlled with .env

--- src/apis/url.api.ts
+++ src/apis/url.api.ts
@@ -1,5 +1,3 @@
-export const BASE_URL = ""
-
 export const ENDPOINT = {
   KLINE: "/proxy-api/api/v3/klines",
   TICKER: "/proxy-api/api/v3/ticker/24hr",
--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/proxy-api': {
-        target: 'https://api.binance.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/proxy-api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/proxy-api': {
+          target: env.VITE_BINANCE_REST_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/proxy-api/, ''),
+        },
+      },
+    },
+  };
 });
