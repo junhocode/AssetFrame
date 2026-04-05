9344cf83414fc8feb74a72a8593f470e8ffa4433
진준호 <theworstvalentine@gmail.com>
2026-04-05 17:44:08 +0900

    feat: Add connecting status to SystemStatus

--- src/components/SystemStatus/SystemStatus.styles.ts
+++ src/components/SystemStatus/SystemStatus.styles.ts
@@ -1,9 +1,11 @@
-export const statusContainer = "flex items-center gap-2.5"
+export const statusContainer = "flex items-center gap-2.5 shrink-0";
 
-export const text = "text-sm text-foreground"
+export const text = "text-sm text-foreground whitespace-nowrap";
 
 export const connectedIconPing = "animate-ping absolute h-full w-full rounded-full bg-green-500 opacity-75";
 
-export const connectedIcon = "w-2 h-2 rounded-full bg-green-500"
+export const connectedIcon = "w-2 h-2 rounded-full bg-green-500";
 
-export const disconnectedIcon = "w-2 h-2 rounded-full bg-red-500"
+export const connectingIcon = "w-2 h-2 rounded-full bg-yellow-500";
+
+export const disconnectedIcon = "w-2 h-2 rounded-full bg-red-500";
--- src/components/SystemStatus/SystemStatus.tsx
+++ src/components/SystemStatus/SystemStatus.tsx
@@ -3,21 +3,21 @@ import { wsStatusAtom } from "@/atoms/wsStatusAtom";
 import * as S from "./SystemStatus.styles";
 
 export const SystemStatus = () => {
-  const isConnected = useAtomValue(wsStatusAtom);
-
+  const status = useAtomValue(wsStatusAtom);
 
   return (
     <div className={S.statusContainer}>
       <span className={S.text}>system status</span>
-      {isConnected
-        ?
+      {status === "connected" ? (
         <div className="relative">
           <div className={S.connectedIconPing}></div>
           <div className={S.connectedIcon}></div>
         </div>
-        :
+      ) : status === "connecting" ? (
+        <div className={S.connectingIcon}></div>
+      ) : (
         <div className={S.disconnectedIcon}></div>
-      }
+      )}
     </div>
-  )
+  );
 };
