cff7cd6dfb05ed1c0c2f1d3c5a1cd922703be67f
진준호 <theworstvalentine@gmail.com>
2026-04-05 16:36:26 +0900

    refactor: Simplify with destructuring and named constants

--- src/utils/parseKline.ts
+++ src/utils/parseKline.ts
@@ -1,6 +1,8 @@
 import type { Kline, WSKline } from "@/types/kline.type";
 
 export const parseKline = (wsKline: WSKline["k"]): Kline => {
+  // ws로 받아오는 kline은 캔들 정보읲 포맷으로 파싱해야 한다.
+  // rest의 kline은 캔들 데이터의 형태로 전달된다.
   return [
     wsKline.t,
     wsKline.o,
@@ -11,7 +13,7 @@ export const parseKline = (wsKline: WSKline["k"]): Kline => {
     wsKline.T,
     wsKline.q,
     wsKline.n,
-    "0",
+    "0", // 0은 사옹하지 않는 필드
     "0",
     "0",
   ];
