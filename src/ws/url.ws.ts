77bc5933b1616e4df337e533a0bfeed0d591120e
진준호 <theworstvalentine@gmail.com>
2026-04-05 16:42:19 +0900

    chore: Remove as const from WS Endpoints

--- src/ws/url.ws.ts
+++ src/ws/url.ws.ts
@@ -1,7 +1,7 @@
-export const WS_URL = import.meta.env.VITE_BINANCE_WS_URL;
+const WS_URL = import.meta.env.VITE_BINANCE_WS_URL;
 
 export const WS_ENDPOINTS = {
   orderBook: (symbol: string) => `${WS_URL}/${symbol.toLowerCase()}@depth20@1000ms`,
   trade: (symbol: string) => `${WS_URL}/${symbol.toLowerCase()}@trade`,
   kline: (symbol: string, interval: string) => `${WS_URL}/${symbol.toLowerCase()}@kline_${interval}`
-} as const;
+}
