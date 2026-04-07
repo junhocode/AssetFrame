dbca9a3e73c593d41f136bc1c7918af0d720c98b
junhocode <161407999+junhocode@users.noreply.github.com>
2026-04-07 22:49:39 +0900

    refactor: Remove wrapper div, inline handler

--- src/components/IntervalSelector/IntervalSelector.tsx
+++ src/components/IntervalSelector/IntervalSelector.tsx
@@ -4,31 +4,23 @@ import type { SelectorProps } from "@/types/selector.type";
 import * as S from "./IntervalSelector.styles";
 
 export const IntervalSelector = ({ value, onChange }: SelectorProps) => {
-  const handleValueChange = (newValue: string) => {
-    if (newValue) {
-      onChange(newValue);
-    }
-  };
-
   return (
-    <div>
-      <ToggleGroup
-        type="single"
-        value={value}
-        onValueChange={handleValueChange}
-        className={S.toggleGroup}
-      >
-        {INTERVALS.map((interval) => (
-          <ToggleGroupItem
-            key={interval}
-            value={interval}
-            aria-label={`Select ${interval}`}
-            className={S.toggleGroupItems}
-          >
-            {interval}
-          </ToggleGroupItem>
-        ))}
-      </ToggleGroup>
-    </div>
+    <ToggleGroup
+      type="single"
+      value={value}
+      onValueChange={(v) => v && onChange(v)}
+      className={S.toggleGroup}
+    >
+      {INTERVALS.map((interval) => (
+        <ToggleGroupItem
+          key={interval}
+          value={interval}
+          aria-label={`Select ${interval}`}
+          className={S.toggleGroupItems}
+        >
+          {interval}
+        </ToggleGroupItem>
+      ))}
+    </ToggleGroup>
   );
 };
