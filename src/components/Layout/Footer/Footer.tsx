bc5e5f57eb826868a0a080bb60556dc1e8196ead
junhocode <161407999+junhocode@users.noreply.github.com>
2026-04-07 22:48:53 +0900

    fix: Use semantic <footer> tag

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -2,11 +2,11 @@ import * as S from "./Footer.styles";
 
 export const Footer = () => {
   return (
-    <div className={S.footer}>
+    <footer className={S.footer}>
       <div className={S.text}>
         Copyright © 2025 Developed by JunhoCode, Powered by
       </div>
       <img src="/Binance-Logo.png" alt="Binance" className={S.image} />
-    </div>
+    </footer>
   );
 };
